b76fea01b82fa089c5d4ae051eafea1ac08b3cf2
evgenijmartynuk07 <evgenijmartynuk07@gmail.com>
2024-03-02 16:59:00 +0200

    env

--- movie_project/settings.py
+++ movie_project/settings.py
@@ -22,12 +22,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-)9a@%d7hj8%r0o%v=1s27l(13an)c_9o#a@c$*izg30=5hllc1"
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", None)
+
+# ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 
 # Application definition
