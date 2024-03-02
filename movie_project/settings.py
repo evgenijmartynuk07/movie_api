e286999d5bdc29b3202b42c07a8d72ca3c11d36f
evgenijmartynuk07 <evgenijmartynuk07@gmail.com>
2024-03-02 17:02:07 +0200

    env

--- movie_project/settings.py
+++ movie_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", None)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost,oyster-app-me2uu.ondigitalocean.app").split(",")
 
 
 # Application definition
